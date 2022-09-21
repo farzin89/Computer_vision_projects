2eb1c9cd6a923e88bf515b507eed2eccd1aacd0a
farzin89 <farzin_ahmadi90@yahoo.com>
2022-09-21 09:49:44 +0200

    circle detection(install libraries,VideoCapture,convert to gray

---
+++ circle detection/circle_detection.py
@@ -0,0 +1,21 @@
+
+import cv2
+import numpy as np
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+while(True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray = cv2.medianBlur(gray,5)
+
+
+
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('gray', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
