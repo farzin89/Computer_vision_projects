eb92dfeb6af1e8ac33d81efcaecc8de24cb05034
farzin89 <farzin_ahmadi90@yahoo.com>
2022-09-22 08:06:13 +0200

    finding reagon of interest

--- self_driving car/self_driving_car.py
+++ self_driving car/self_driving_car.py
@@ -1,24 +1,31 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
+def canny(image):
+    #convert to gray
+    gray = cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY)
+    #convert to blur
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+    #apply canny to reduce the noice
+    canny = cv2.Canny(blur,50,150)
+    return canny
+
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([[(200,height),(1100,height),(550,250)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask,polygons,255)
+    return mask
+
+
 
 image = cv2.imread('road.JPG')
 lane_image = np.copy(image)
-#convert to gray
-gray = cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY)
-#convert to blur
-blur = cv2.GaussianBlur(gray,(5,5),0)
-#apply canny to reduce the noice
-canny = cv2.Canny(blur,50,150)
-
-
-cv2.imshow('result',canny)
+canny = canny(lane_image)
 
 
 
 
-
-
-
-
-
+cv2.imshow('result',region_of_interest(canny))
 cv2.waitKey(0)
