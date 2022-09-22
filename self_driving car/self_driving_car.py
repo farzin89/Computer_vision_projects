cff24ec2d959417d5a0069fb775a9111d64acea7
farzin89 <farzin_ahmadi90@yahoo.com>
2022-09-22 07:41:19 +0200

    apply gray,blur and canny function

--- self_driving car/self_driving_car.py
+++ self_driving car/self_driving_car.py
@@ -1,3 +1,24 @@
 import cv2
+import numpy as np
 
-image
+image = cv2.imread('road.JPG')
+lane_image = np.copy(image)
+#convert to gray
+gray = cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY)
+#convert to blur
+blur = cv2.GaussianBlur(gray,(5,5),0)
+#apply canny to reduce the noice
+canny = cv2.Canny(blur,50,150)
+
+
+cv2.imshow('result',canny)
+
+
+
+
+
+
+
+
+
+cv2.waitKey(0)
