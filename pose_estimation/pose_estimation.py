e2284c6538a679e71e6cebb952c5791a3c858d78
farzin89 <farzin_ahmadi90@yahoo.com>
2022-10-01 12:27:40 +0200

    draw landmarks

--- pose_estimation/pose_estimation.py
+++ pose_estimation/pose_estimation.py
@@ -3,6 +3,7 @@ import cv2
 import mediapipe as mp
 import time
 
+mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
@@ -11,7 +12,11 @@ pTime = 0
 while True:
     success,img = cap.read()
     imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    
+    result = pose.process(imgRGB)
+    print(result.pose_landmarks)
+    if result.pose_landmarks:
+        mpDraw.draw_landmarks(img,result.pose_landmarks)
+
 
 
     cTime = time.time()
